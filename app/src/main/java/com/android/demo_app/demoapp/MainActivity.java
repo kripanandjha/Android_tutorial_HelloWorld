2ec772d87ce191b87fcc873b9f8a4eb7331f0e08
kripanandjha <jha.kripanand@gmail.com>
2018-07-15 23:11:34 -0700

    Adding a login username and password buttons

--- app/src/main/java/com/android/demo_app/demoapp/MainActivity.java
+++ app/src/main/java/com/android/demo_app/demoapp/MainActivity.java
@@ -4,11 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
-        Log.i("Info","Button pressed");
+
+        // When button pressed print what is in the text field
+        EditText myText = (EditText) findViewById(R.id.userTextField);
+        EditText myTextp = (EditText) findViewById(R.id.passwordField);
+        Log.i("Info","Button pressed, textField value "+myText.getText().toString() + " and Password "+myTextp.getText().toString() );
     }
 
 
