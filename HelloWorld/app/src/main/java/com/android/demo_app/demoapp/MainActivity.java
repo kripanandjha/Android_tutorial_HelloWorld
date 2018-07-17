42f440265cd7c4e25c63804d154410572f5ff33e
kripanandjha <jha.kripanand@gmail.com>
2018-07-16 20:58:14 -0700

    moving files to sub folder

--- HelloWorld/app/src/main/java/com/android/demo_app/demoapp/MainActivity.java
+++ HelloWorld/app/src/main/java/com/android/demo_app/demoapp/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this,"Hello "+myText.getText().toString(),Toast.LENGTH_SHORT).show();
 
         //Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 
 
